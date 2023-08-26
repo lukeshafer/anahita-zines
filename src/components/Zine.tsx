cda72ab45b22426536d5cf37e82ce3fbdb3c22d6
Luke Shafer <lukealanshafer@gmail.com>
2023-08-26 17:44:23 -0400

    Improve page transitions for zines

--- src/components/Zine.tsx
+++ src/components/Zine.tsx
@@ -1,4 +1,4 @@
-import { createMemo, createSignal, createContext, useContext, JSX, createEffect, onMount } from "solid-js";
+import { createMemo, createSignal, createContext, useContext, JSX, createEffect, onMount, Show, Switch, Match } from "solid-js";
 import { startViewTransition } from "../lib";
 import type { Zine } from "../config";
 import { RATIO, WIDTH, POSITIONS, LAST_PAGE } from "../constants"
@@ -6,6 +6,7 @@ import { RATIO, WIDTH, POSITIONS, LAST_PAGE } from "../constants"
 const ZineContext = createContext({
 	positions: POSITIONS as readonly string[],
 	title: "",
+	transitionState: ""
 });
 
 export default function Zine(props: { zine: Zine, title: string }) {
@@ -17,59 +18,70 @@ export default function Zine(props: { zine: Zine, title: string }) {
 		? POSITIONS.slice().reverse()
 		: POSITIONS);
 
-	const [currentPage, setCurrentPage] = createSignal(0);
-	const posIndex = (() =>
-		(2 * currentPage() + startsIn() - 1) % positions().length);
+	const [transitionState, setTransitionState] = createSignal("");
 
-	const rightPage = (() => currentPage() === LAST_PAGE ? -1 : posIndex())
-	const leftPage = (() => currentPage() === 0 ? -1 : (posIndex() - 1 + positions().length) % positions().length)
+	const [currentPage, setCurrentPage] = createSignal(0);
+	const posIndex = ((offset = 0) =>
+		(2 * (currentPage() + offset) + startsIn() - 1) % positions().length);
+
+	const rightPage = ((offset = 0) => currentPage() + offset === LAST_PAGE ? -1 : posIndex(offset))
+	const leftPage = ((offset = 0) => currentPage() + offset === 0 ? -1 : (posIndex(offset) - 1 + positions().length) % positions().length)
 
 	onMount(() => {
 		window.addEventListener('keydown', (e) => {
-			console.log(e.key)
-			if (e.key === "ArrowLeft") {
-				prev()
-			} else if (e.key === "ArrowRight") {
-				next()
-			}
+			if (e.key === "ArrowLeft") prev()
+			else if (e.key === "ArrowRight") next()
+
 		})
 	})
 
+	let timeout: number;
 	function next() {
-		console.log("next");
-		startViewTransition(() => {
-			setCurrentPage((page) => {
-				if (page === LAST_PAGE) {
-					return 0;
-				} else return page + 1;
-			})
-		})
+		if (currentPage() === LAST_PAGE || transitionState()) return
+		const update = () => {
+			setTransitionState("")
+			setCurrentPage((page) => page === LAST_PAGE ? page : page + 1)
+		}
+
+		if (transitionState()) {
+			clearTimeout(timeout)
+			update()
+		}
+
+		setTransitionState("turning-right")
+		timeout = setTimeout(update, 500)
 	}
 
 	function prev() {
-		console.log("prev");
-		startViewTransition(() => {
-			setCurrentPage((page) => {
-				if (page === 0) {
-					return LAST_PAGE;
-				} else return page - 1;
-			})
-		})
+		if (currentPage() === 0 || transitionState()) return
+		const update = () => {
+			setTransitionState("")
+			setCurrentPage((page) => page === 0 ? page : page - 1)
+		}
+
+		if (transitionState()) {
+			clearTimeout(timeout)
+			update()
+		}
+
+		setTransitionState("turning-left")
+		timeout = setTimeout(update, 500)
 	}
 
 	return (
 		<ZineContext.Provider value={{
 			get positions() { return positions() },
 			get title() { return props.title },
+			get transitionState() { return transitionState() }
 		}}>
-			<div class="flex pb-3">
+			<div class="flex pb-3 relative">
 				<ZinePage
 					src={props.zine.src}
 					position={leftPage()}
 					page={2 * currentPage()}
 					side="left"
 					reversed={props.zine.reversed ?? false}>
-					<Button onClick={prev} side="left" title="Previous">{'<'}</Button>
+					{currentPage() !== 0 ? <Button onClick={prev} side="left" title="Previous">{'<'}</Button> : null}
 				</ZinePage>
 				<ZinePage
 					src={props.zine.src}
@@ -77,8 +89,48 @@ export default function Zine(props: { zine: Zine, title: string }) {
 					page={2 * currentPage() + 1}
 					side="right"
 					reversed={props.zine.reversed ?? false}>
-					<Button onClick={next} side="right" title="Next">{'>'}</Button>
+					{currentPage() !== LAST_PAGE ? <Button onClick={next} side="right" title="Next">{'>'}</Button> : null}
 				</ZinePage>
+				<Switch>
+					<Match when={transitionState() === "turning-left"}>
+						<div class="absolute inset-0 ">
+							<ZinePage
+								src={props.zine.src}
+								position={leftPage(-1)}
+								side="left"
+								transitionIn
+								reversed={props.zine.reversed ?? false}>
+							</ZinePage>
+							<ZinePage
+								src={props.zine.src}
+								position={rightPage(-1)}
+								side="right"
+								transitionIn
+								reversed={props.zine.reversed ?? false}>
+							</ZinePage>
+						</div>
+					</Match>
+					<Match when={transitionState() === "turning-right"}>
+						<div class="absolute inset-0 ">
+							<ZinePage
+								src={props.zine.src}
+								position={leftPage(1)}
+								page={2 * currentPage()}
+								side="left"
+								transitionIn
+								reversed={props.zine.reversed ?? false}>
+							</ZinePage>
+							<ZinePage
+								src={props.zine.src}
+								position={rightPage(1)}
+								page={2 * currentPage() + 1}
+								side="right"
+								transitionIn
+								reversed={props.zine.reversed ?? false}>
+							</ZinePage>
+						</div>
+					</Match>
+				</Switch>
 			</div>
 		</ZineContext.Provider>
 	);
@@ -87,9 +139,10 @@ export default function Zine(props: { zine: Zine, title: string }) {
 export function ZinePage(props: {
 	src: string;
 	position: number;
-	page: number;
+	page?: number;
 	reversed: boolean;
 	side: "left" | "right";
+	transitionIn?: boolean;
 	children: JSX.Element;
 }) {
 	const context = useContext(ZineContext);
@@ -100,7 +153,20 @@ export function ZinePage(props: {
 		props.position >= context.positions.length / 2
 	);
 	return (
-		<div class="zine-page relative outline outline-2 outline-black/10 group" style={{ 'view-transition-name': viewTransitionName() }}>
+		<div
+			class="zine-page outline outline-2 outline-black/10 group top-0"
+			style={{
+				'view-transition-name': viewTransitionName(),
+				position: props.transitionIn ? "absolute" : "relative"
+			}}
+			classList={{
+				"page-right-old": props.side === "right" && context.transitionState === "turning-right" && !props.transitionIn,
+				"page-left-old": props.side === "left" && context.transitionState === "turning-left" && !props.transitionIn,
+				"page-right-new": props.side === "right" && context.transitionState === "turning-left" && props.transitionIn,
+				"page-left-new": props.side === "left" && context.transitionState === "turning-right" && props.transitionIn,
+				"left-0": props.side === "left",
+				"right-0": props.side === "right",
+			}}>
 			<div
 				class="flex flex-col items-center justify-center bg-center bg-no-repeat w-[47vw]"
 				style={{
@@ -116,7 +182,7 @@ export function ZinePage(props: {
 			</div>
 			{props.children}
 			{
-				props.page > 0 && props.page <= LAST_PAGE * 2 ?
+				props.page && props.page > 0 && props.page <= LAST_PAGE * 2 ?
 					<p class="text-center w-full absolute">{props.page}</p>
 					: null
 			}
